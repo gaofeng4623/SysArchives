5b77b2917e18ce983f04b88c228ccac11b17669b
gf <gaofen_888@163.com>
2017-09-01 17:11:30 +0800

    初始化提交项目

---
+++ common/src/main/java/common/Common.java
@@ -0,0 +1,13 @@
+package common;
+
+/**
+ * Created by Administrator on 2017/1/2.
+ */
+public class Common {
+    public String getResult() {
+        return "common模块.........";
+    }
+    public static void main(String[] args){
+        System.out.println(args.toString());
+    }
+}
